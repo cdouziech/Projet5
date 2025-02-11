37ff6a614cb37967dc2337bb20356f8bac56c9e5
Clotaire Douziech <clotairedouziech@gmail.com>
2025-02-11 12:49:16 +0100

    Route install and first routes

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,5 @@
+function ErrorPage(){
+    return (<h1>La page à laquelle vous essayez d&apos;accéder n&apos;existe pas :/</h1>);
+}
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import ErrorPage from './ErrorPage.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/testRouting" element={<div>Routing is Working</div>} />
+      <Route path='*' element={<ErrorPage />}/>
+    </Routes>
+  </BrowserRouter>
 )
