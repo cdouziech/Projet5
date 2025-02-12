3f6aab9cb1fceb2551d5704287fded65e0bb654b
Clotaire Douziech <clotairedouziech@gmail.com>
2025-02-12 19:07:00 +0100

    creation of main repo structure with 'components' and 'pages'

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import ErrorPage from './ErrorPage.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/testRouting" element={<div>Routing is Working</div>} />
-      <Route path='*' element={<ErrorPage />}/>
-    </Routes>
-  </BrowserRouter>
+	<BrowserRouter>
+		<Header/>	
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/logements" element={<Logements />} />
+			<Route path='*' element={<ErrorPage />}/>
+		</Routes>
+		<Footer/>
+	</BrowserRouter>
 )
